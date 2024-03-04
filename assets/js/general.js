7669133ec114c6ba7cb1bc74cebf5c9bbf16424c
connymv <connymariscalvelasquez@gmail.com>
2024-03-04 09:52:46 -0600

    4 de marzo del 2024

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,3 +1,11 @@
 function inicio(){
-    document.location.href = "/index.html"
+    document.location.href = "/index.html";
 }
+
+function redirectToSpotify(){
+    window.location.href = 'https://www.spotify.com';
+  }
+
+function redirectTologin(){
+    window.location.href = "./conny/conny.html";
+  }
