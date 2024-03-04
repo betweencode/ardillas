306c8a37833e78fd5710c2b57e7e3bde2c0280db
JESSIMV <jessy_kmv@hotmail.com>
2024-03-04 10:04:10 -0600

    2doAvance

--- assets/js/JMV/JMV.js
+++ assets/js/JMV/JMV.js
@@ -1,3 +1,3 @@
 function saludando(){
-    alert("Hola soy jessi");
+    alert ("Hola");
 }
--- assets/js/general.js
+++ assets/js/general.js
@@ -1,3 +1,7 @@
 function inicio(){
     document.location.href = "/index.html"
 }
+
+function sobre(){
+    window.location.href = "./JMV/sobre.html"
+}
