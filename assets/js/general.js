d489e9d2dce3f9063e06ea965422e0b3e92f956d
connymv <connymariscalvelasquez@gmail.com>
2024-03-04 22:20:31 -0600

    proyecto

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,13 +1,10 @@
 function inicio(){
-<<<<<<< HEAD
+
     document.location.href = "/index.html"
 }
 
 function sobre(){
     window.location.href = "./JMV/sobre.html"
-}
-=======
-    document.location.href = "/index.html";
 }
 
 function redirectToSpotify(){
@@ -17,4 +14,4 @@ function redirectToSpotify(){
 function redirectTologin(){
     window.location.href = "./conny/conny.html";
   }
->>>>>>> 7669133ec114c6ba7cb1bc74cebf5c9bbf16424c
+
